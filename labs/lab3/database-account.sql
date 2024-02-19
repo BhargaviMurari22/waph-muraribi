da08d05430cec570c84ef8672f3e68297983f446
BhargaviMurari22 <muraribi@mail.uc.edu>
2024-02-19 17:12:38 -0500

    Updated files

---
+++ labs/lab3/database-account.sql
@@ -0,0 +1,4 @@
+create database waph;
+
+CREATE USER 'muraribi'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
+GRANT ALL ON waph.* TO 'muraribi'@'localhost';
---
+++ labs/lab3/index.php
@@ -0,0 +1,26 @@
+<?php
+	session_start();    
+	if (checklogin_mysql($_POST["username"],$_POST["password"])) {
+?>
+	<h2> Welcome <?php echo $_POST['username']; ?> !</h2>
+<?php		
+	}else{
+		echo "<script>alert('Invalid username/password');window.location='form.php';</script>";
+		die();
+	}
+	function checklogin_mysql($username, $password) {
+		$mysqli= new mysqli('localhost','kamatikn','Pa$$w0rd','waph');
+		if($mysqli-> connect_errno){
+			printf("Database connection failed");
+			exit();
+		}
+		$sql="SELECT * FROM users WHERE username='" . $username . "' ";
+		$sql= $sql . " AND password = md5('" . $password . "')";
+
+		$result=$mysqli->query($sql);
+		if($result->num_rows==1) 
+		  return TRUE;
+		else 
+		  return FALSE;
+  	}
+?>
