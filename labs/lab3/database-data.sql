dfbcbfa872be5f4a705392d818e22a03978022d7
BhargaviMurari22 <muraribi@mail.uc.edu>
2024-04-09 17:14:33 -0400

    Updated files

---
+++ labs/lab3/database-data.sql
@@ -0,0 +1,2 @@
+
+insert into users(username,password) values('admin',md5('Pa$$w0rd'));
--- labs/lab3/index.php
+++ labs/lab3/index.php
@@ -2,22 +2,24 @@
 	session_start();    
 	if (checklogin_mysql($_POST["username"],$_POST["password"])) {
 ?>
-	<h2> Welcome <?php echo $_POST['username']; ?> !</h2>
+	<h2> Welcome <?php echo htmlentities($_POST['username']); ?> !</h2>
 <?php		
 	}else{
 		echo "<script>alert('Invalid username/password');window.location='form.php';</script>";
 		die();
 	}
 	function checklogin_mysql($username, $password) {
-		$mysqli= new mysqli('localhost','kamatikn','Pa$$w0rd','waph');
+		$mysqli= new mysqli('localhost','muraribi','Pa$$w0rd','waph');
 		if($mysqli-> connect_errno){
 			printf("Database connection failed");
 			exit();
 		}
-		$sql="SELECT * FROM users WHERE username='" . $username . "' ";
-		$sql= $sql . " AND password = md5('" . $password . "')";
-
-		$result=$mysqli->query($sql);
+		$prepared_sql= "select * from users where username=? " . " AND password=md5(?);";
+		$stmt=$mysqli->prepare($prepared_sql);
+		$stmt->bind_param("ss",$username,$password);
+		$stmt-> execute();
+		$result= $stmt->get_result();
+		echo "DEBUG->sql=$sql";
 		if($result->num_rows==1) 
 		  return TRUE;
 		else 
